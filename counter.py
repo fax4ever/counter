90b0eed7dc88f1e6fe0f2b2e804657f6f625e215
Fabio Massimo Ercoli <fabiomassimo.ercoli@gmail.com>
2023-02-15 10:48:30 +0100

    Evolve the program

--- counter.py
+++ counter.py
@@ -1,10 +1,22 @@
 #!/usr/bin/env python3
 
+import json
+
 words = {}
+max_word = ""
+max_count = 0
 
 with open("infinispan.txt", "r") as inputFile:
     for line in inputFile:
         for word in line.split():
             words[word] = words.get(word, 0) + 1
+            if (words[word] > max_count):
+                max_word = word
+                max_count = words[word]
 
 print(words)
+
+with open("words.json", "w") as dump:
+        json.dump(words, dump, indent="\t")
+
+print("max word: '{}' - max count: {}".format(max_word, max_count))          
